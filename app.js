2dfed3df92f242fcb674b2a7e7f03e487758de6e
dcheng34551 <dcheng34551@gmail.com>
2021-03-03 22:36:23 +0800

    second week assignment

---
+++ app.js
@@ -0,0 +1,27 @@
+//Request 1: Click to Change Text
+const heroBackground = document.querySelector('.hero-bg');
+
+heroBackground.addEventListener('click', () => {
+    heroBackground.innerHTML = `<h1>Have a Good Time</h1>`
+});
+
+//Request 2: Click to Show/Close Menu
+const hamburger = document.querySelector('.hamburger');
+const menu = document.querySelector('.menu');
+const closeBtn = document.querySelector('#close-btn');
+
+hamburger.addEventListener('click', () => {
+    menu.classList.remove('hidden-menu');
+})
+
+closeBtn.addEventListener('click', () => {
+    menu.classList.add('hidden-menu');
+})
+
+//Request 3: Click to Show More Content Boxes
+const actionBtn = document.querySelector('#action-btn');
+const hiddenItems = document.querySelector('.hidden-items');
+
+actionBtn.addEventListener('click', () => {
+    hiddenItems.classList.remove('hidden-items')
+})
