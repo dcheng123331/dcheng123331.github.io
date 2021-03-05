2cb4b919f2301aadb40d8137bc12e17f177caeec
dcheng34551 <dcheng34551@gmail.com>
2021-03-05 16:06:03 +0800

    dynimacally load content boxes & change the direction of the animation(menu)

--- app.js
+++ app.js
@@ -19,9 +19,54 @@ closeBtn.addEventListener('click', () => {
 })
 
 //Request 3: Click to Show More Content Boxes
+const itemList = [
+    {title: "Context Box 1"},
+    {title: "Context Box 2"},
+    {title: "Context Box 3"},
+    {title: "Context Box 4"},
+    {title: "Context Box 5"},
+    {title: "Context Box 6"},
+    {title: "Context Box 7"},
+    {title: "Context Box 8"},
+    {title: "Context Box 9"},
+    {title: "Context Box 10"},
+    {title: "Context Box 11"},
+]
 const actionBtn = document.querySelector('#action-btn');
-const hiddenItems = document.querySelector('.hidden-items');
+// const hiddenItems = document.querySelector('.hidden-items');
+
+// actionBtn.addEventListener('click', () => {
+//     hiddenItems.classList.remove('hidden-items')
+// })
+
+const firstLoadedBox = document.querySelector('.content-items');
+
+function createContentBox(content) {
+    const contentBox = `
+        <div class="box">
+            <p>${content.title}</p>
+        </div>
+    `;
+    return contentBox;
+};
+
+// loading initial contents
+let contentBoxIndex = 0;
+
+for(let i = 0; i <= 3; i++) {
+    const boxItem = createContentBox(itemList[i]);
+    firstLoadedBox.innerHTML += boxItem;
+    contentBoxIndex++
+};
 
 actionBtn.addEventListener('click', () => {
-    hiddenItems.classList.remove('hidden-items')
-})
+    let fourMoreBoxes = (contentBoxIndex + 4);
+    if (fourMoreBoxes > itemList.length) {
+        fourMoreBoxes = itemList.length;
+    }
+    for(let i = contentBoxIndex; i < fourMoreBoxes; i++) {
+        const boxItem = createContentBox(itemList[i]);
+        firstLoadedBox.innerHTML += boxItem;
+        contentBoxIndex++
+    }
+});
