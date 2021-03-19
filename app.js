9b59e75320f992337ad8e0fd47d26a8dd1b295fb
dcheng34551 <dcheng34551@gmail.com>
2021-03-19 15:35:08 +0800

    react version - replace class with className

--- app.js
+++ app.js
@@ -7,18 +7,18 @@ const Nav = () => {
     return (
         <div>
             <nav>
-                <div class="logo"><a href="/">Website Title / Logo</a></div>
-                <div class="nav-items">
-                    <div class="hamburger" onClick={() => setHidden(!hidden)}>
-                        <div class="bar"></div>
+                <div className="logo"><a href="/">Website Title / Logo</a></div>
+                <div className="nav-items">
+                    <div className="hamburger" onClick={() => setHidden(!hidden)}>
+                        <div className="bar"></div>
                     </div>
-                    <ul class="large-nav">
+                    <ul className="large-nav">
                         <li><a href="/">Item1</a></li>
                         <li><a href="/">Item2</a></li>
                         <li><a href="/">Item3</a></li>
                         <li><a href="/">Item4</a></li>
                     </ul>
-                    <div className={`menu ${hidden ? 'hidden-menu' : '' }`}>
+                    <div classNameName={`menu ${hidden ? 'hidden-menu' : '' }`}>
                         <ul>
                             <li><a href="/">Item1</a></li>
                             <li><a href="/">Item2</a></li>
@@ -38,7 +38,7 @@ const Hero = () => {
     const [text, setText] = useState('Welcome Message');
 
     return (
-        <div class="hero-bg">
+        <div className="hero-bg">
             <h1 onClick={() => setText('Have a good time')}>{text}</h1>
         </div>
     )
@@ -49,34 +49,34 @@ const Content = () => {
     const [show, setShow] = useState(true);
 
     return (
-        <div class="content">
+        <div className="content">
             <h2>Section Title</h2>
-            <div class="content-items">
-                <div class="box box1">
+            <div className="content-items">
+                <div className="box box1">
                     <p>Content Box 1</p>
                 </div>
-                <div class="box box2">
+                <div className="box box2">
                     <p>Content Box 2</p>
                 </div>
-                <div class="box box3">
+                <div className="box box3">
                     <p>Content Box 3</p>
                 </div>
-                <div class="box box4">
+                <div className="box box4">
                     <p>Content Box 4</p>
                 </div>
             </div>
             <button id="action-btn" onClick={() => setShow(false)}>Call to Action</button>
-            <div class={`content-items ${show ? 'hidden-items' : ''}`}>
-                <div class="box box5">
+            <div className={`content-items ${show ? 'hidden-items' : ''}`}>
+                <div className="box box5">
                     <p>Content Box 5</p>
                 </div>
-                <div class="box box6">
+                <div className="box box6">
                     <p>Content Box 6</p>
                 </div>
-                <div class="box box7">
+                <div className="box box7">
                     <p>Content Box 7</p>
                 </div>
-                <div class="box box8">
+                <div className="box box8">
                     <p>Content Box 8</p>
                 </div>
             </div>
@@ -86,7 +86,7 @@ const Content = () => {
 
 function App() {
     return (
-      <div className="App">
+      <div classNameName="App">
         <Nav />
         <Hero />
         <Content />
